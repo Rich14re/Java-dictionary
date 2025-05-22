92a21d0e0d44448dd729a441d39b169e3cf27d4a
Rich14re <84147059+Rich14re@users.noreply.github.com>
2025-05-22 19:19:52 +0300

    add, remove, print, save methods created for AbstractDiict

--- AbstractDictionary.java
+++ AbstractDictionary.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 abstract class AbstractDictionary {
@@ -26,7 +29,52 @@ abstract class AbstractDictionary {
             }
             fileReader.close();
         } catch(FileNotFoundException e) {
-            System.out.println("Файл не найден!");
+            System.out.println("Файл не найден");
+        }
+    }
+
+    boolean add(String word, String trans) {
+        if(checkWord(word)) {
+            words.put(word, trans);
+            save();
+            return true;
+        }
+        return false;
+    }
+
+    boolean remove(String word) {
+        if(words.remove(word) != null) {
+            save();
+            return true;
+        }
+        return false;
+    }
+
+    String find(String word) {
+        return words.get(word);
+    }
+
+    void print() {
+        if(words.isEmpty()) {
+            System.out.println("Словарь пуст");
+            return;
+        }
+
+        int i = 1;
+        for(Map.Entry<String, String> entry : words.entrySet()) {
+            System.out.println(i++ + ". " + entry.getKey() + " - " + entry.getValue());
+        }
+    }
+
+    void save() {
+        try {
+            PrintWriter writer = new PrintWriter(file);
+            for(Map.Entry<String, String> entry : words.entrySet()) {
+                writer.println(entry.getKey() + " - " + entry.getValue());
+            }
+            writer.close();
+        } catch(IOException e) {
+            System.out.println("Ошибка при сохранении");
         }
     }
 }
